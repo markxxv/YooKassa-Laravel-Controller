4b661e55345df4a881df1d93d65063187116101d
markxxv <mbogrov@gmail.com>
2021-05-26 21:54:13 +0200

    Update PaymentController.php

--- PaymentController.php
+++ PaymentController.php
@@ -16,55 +16,55 @@ class PaymentController extends Controller
     public function payCreate(Request $req)
     {
 
-   		$clientId = 'ВАШ id МАГАЗИНА';
-   		$clientSecret = 'ВАШ КЛЮЧЬ API';
+   	$clientId = 'ВАШ id МАГАЗИНА';
+   	$clientSecret = 'ВАШ КЛЮЧЬ API';
 
-   		$client = new Client();
+   	$client = new Client();
     	$client->setAuth($clientId, $clientSecret);
 
     	$payment = $client->createPayment([
- 			'amount' => [
-	        'value' => 102,
-	        'currency' => 'RUB',
-	    	],
-	    	'description' => 'Заказ №72', // Прописываем нужное описание
-	    	'capture' => true,
-	    	'confirmation' => [
-	        'type' => 'redirect',
+ 	    'amount' => [
+	         'value' => 102,
+	         'currency' => 'RUB',
+	    ],
+	    'description' => 'Заказ №72', // Прописываем нужное описание
+	    'capture' => true,
+	    'confirmation' => [
+	    'type' => 'redirect',
 	        'return_url' => route('pay.callback'), // Задаём страницу на которую пользователь вернётся если нажмёт книпку вернутся в магазин на сайте yooMoney
-	      ],
-	      'metadata' => [
-	        'order_id' => 1, // Передаём номер заказа например через $rec->amount
-	      ],
-	 	], uniqid('', true));
-
+	    ],
+            'metadata' => [
+	         'order_id' => 1, // Передаём номер заказа например через $rec->amount
+            ],
+	], uniqid('', true));
 
     	return redirect( $payment->getConfirmation()->getConfirmationUrl() );
+
     }
+
 
     public function payCallback(Request $req)
     {
     	if ($req->event == 'payment.succeeded' && $req->object['status'] == 'succeeded') {
-    		if ($req->object['paid'] === true) {
-          # Запись ошибок в лог
-    		  info( json_encode($req->object) ); 
+    	   if ($req->object['paid'] === true) {
+              # Запись ошибок в лог
+    	      info( json_encode($req->object) ); 
           
-          # Пример обновления статуса заказа в соответствующей модели
-    			// $order = Order::find($req->object['metadata']['order_id']); 
-		      //    $order->payment = 'payed';
-		      //    $order->payment_system = 'paypal';
-		      //    $order->save();
-
-    		}
+              # Пример обновления статуса заказа в соответствующей модели
+    	      // $order = Order::find($req->object['metadata']['order_id']); 
+	      //    $order->payment = 'payed';
+	      //    $order->payment_system = 'paypal';
+	      //    $order->save();
+    	   }
     	} else {
-        # Запись ошибок в лог
-    		info( json_encode($req->object) ); 
+            # Запись ошибок в лог
+    	    info( json_encode($req->object) ); 
     		
-        # Пример записи ответа в модель заказа
-        // $order = Order::find($req->object['metadata']['order_id']); 
-		    //    $order->payment = 'error';
-		    //    $order->payment_message = json_encode($req->object);
-		    //    $order->save();
+            # Пример записи ответа в модель заказа
+            // $order = Order::find($req->object['metadata']['order_id']); 
+	    //    $order->payment = 'error';
+	    //    $order->payment_message = json_encode($req->object);
+	    //    $order->save();
     	}
 
     }
